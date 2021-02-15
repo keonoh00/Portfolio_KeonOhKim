c7e44d79f8ac16439cb7216f2df31befd67102b5
Keon Oh Kim <keonoh00@gmail.com>
2021-02-15 20:04:43 +0900

    Inserted Scrapper for weather and saved on data.csv

--- Python_Project/Project_LotteryMachineLearning/Data_Caller.py
+++ Python_Project/Project_LotteryMachineLearning/Data_Caller.py
@@ -10,6 +10,8 @@ import pandas as pd
 import requests
 from tqdm import tqdm
 import json
+from bs4 import BeautifulSoup
+import re
 
 
 # Function to call the previous lottery data
@@ -23,11 +25,9 @@ def getLottoWinInfo(minDrwNo, maxDrwNo):
     drwtNo6 = []
     bnusNo = []
     drwNo = []
-    totSellamnt = []
-    drwNoDate = []
-    firstAccumamnt = []
-    firstPrzwnerCo = []
-    firstWinamnt = []
+    drwYear = []
+    drwMon = []
+    drwDay = []
 
     for i in tqdm(range(minDrwNo, maxDrwNo+1, 1)):
         req_url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + str(i)
@@ -41,20 +41,60 @@ def getLottoWinInfo(minDrwNo, maxDrwNo):
         drwtNo5.append(lottoNo['drwtNo5'])
         drwtNo6.append(lottoNo['drwtNo6'])
         bnusNo.append(lottoNo['bnusNo'])
-        totSellamnt.append(lottoNo['totSellamnt'])
-        drwNoDate.append(lottoNo['drwNoDate'])
-        firstAccumamnt.append(lottoNo['firstAccumamnt'])
-        firstPrzwnerCo.append(lottoNo['firstPrzwnerCo'])
-        firstWinamnt.append(lottoNo['firstWinamnt'])
+        drwYear.append(lottoNo['drwNoDate'][:4])
+        drwMon.append(lottoNo['drwNoDate'][5:7])
+        drwDay.append(lottoNo['drwNoDate'][-2:])
 
 
-    lotto_dict = {"Round":drwNo, "DATE":drwNoDate, "Num1":drwtNo1, "Num2":drwtNo2, "Num3":drwtNo3, "Num4":drwtNo4, "Num5":drwtNo5, "Num6":drwtNo6, "bnsNum":bnusNo, "Total_Sold":totSellamnt, "Prize_Tot":firstAccumamnt, "NumWinner":firstPrzwnerCo, "Prize_Ind":firstWinamnt} 
-
-    df_lotto = pd.DataFrame(lotto_dict)
+    lotto_dict = {"Round":drwNo, "Year":drwYear, "Month":drwMon, "Day":drwDay, "Num1":drwtNo1, "Num2":drwtNo2, "Num3":drwtNo3, "Num4":drwtNo4, "Num5":drwtNo5, "Num6":drwtNo6, "bnsNum":bnusNo} 
 
 
-    return df_lotto
+    return lotto_dict
 
 
-DB = getLottoWinInfo(1, 948)
+def getWeather(year, mon, day):
+    URL = f"https://www.weather.go.kr/weather/climate/past_cal.jsp?stn=108&yy={year}&mm={mon}&x=22&y=6&obs=1"
+    web_page = requests.get(URL)
+    soup = BeautifulSoup(web_page.text, 'html.parser')
+    weather_table = soup.find("table", {"class":"table_develop"})
+    table_data = weather_table.find_all("td", {"class":"align_left"})
+    weather_day = []
+    weather_temp = []
+    weather_rain = []
+    for data in table_data:
+        current_string = data.text.strip()
+        if current_string:
+            if current_string[-1] == "일":
+                weather_day.append(current_string[:-1])
+            else:
+                try:
+                    temp = re.search("평균기온:(.+?)℃최고기온", current_string)
+                    rain = re.search("강수량:(.+?)", current_string)
+                    weather_temp.append(temp.group(1))
+                    weather_rain.append(rain.group(1))
+                except:
+                    pass
+    index = weather_day.index(str(int(day)))
+    result = [weather_temp[index], weather_rain[index]]
+    return result
+
+
+min_round = 1
+max_round = 800
+
+print("Getting Lottery Information\n\n\n")
+
+DB = getLottoWinInfo(1, 800)
+temperature = []
+rain = []
+for y, m, d in zip(DB["Year"], DB["Month"], DB["Day"]):
+    print(f"Getting Information... {round(min_round/max_round*100, 2)}%  {min_round}/{max_round}")
+    min_round += 1
+    weather = getWeather(y, m, d)
+    temperature.append(weather[0])
+    rain.append(weather[1])
+DB["Temperature"] = temperature
+DB["RainAmout"] = rain
+
+DB = pd.DataFrame(DB)
 DB.to_csv("Data.csv", index = False)
