8778ddc35fcd2caf5a42c9484ba253a8104edf03
Keon Oh Kim <keonoh00@gmail.com>
2021-02-18 21:13:41 +0900

    Basics of VJS

---
+++ VanilaJS/Chrome_Application/index.js
@@ -0,0 +1,5 @@
+// Alert creates error pop up;
+alert("I have to work hard");
+
+// console.log does not appear on the browser but prints on console
+console.log("You also have to work hard")
---
+++ VanilaJS/Scratch/index.js
@@ -0,0 +1,13 @@
+// Alert creates error pop up;
+alert("I have to work hard");
+
+// console.log does not appear on the browser but prints on console
+console.log("You also have to work hard")
+
+
+// let, var, const exists const cannot be changed
+let A = 221;
+let B = A - 9;
+console.log(B);
+
+/* multi=line comment */
