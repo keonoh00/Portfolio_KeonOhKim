759a436b3024b812530578066fdafd6f48cc1bd5
Keon Oh Kim <keonoh00@gmail.com>
2021-02-20 18:36:39 +0900

    no message

---
+++ VanilaJS/Chrome_Application/userInfo.js
@@ -0,0 +1,15 @@
+// querySelector takes the first selector in CSS way
+// Other method is querySelectorAll takes all the selector and returns the array of the selectors
+const form = document.querySelector(".js-form");
+const userName = fomr.querySelector("input");
+
+function loadName() {
+
+}
+
+
+function init() {
+
+}
+
+init();
